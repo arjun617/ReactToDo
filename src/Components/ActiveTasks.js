f9e2a4312132b4a80b65b96ef653d8896f344faf
NAGARRO\arjundhall <arjun.dhall@nagarro.com>
2022-07-26 17:40:54 +0530

    Add task functionality

--- src/Components/ActiveTasks.js
+++ src/Components/ActiveTasks.js
@@ -2,16 +2,13 @@ import React from "react";
 import ActiveTask from "./ActiveTask";
 
 function ActiveTasks(props) {
-    function addTask(){
-        console.log("Task Added")
-    }
   return (
     <div>
       <h1>Active Tasks</h1>
       {props.tasks.map((task) => {
         return <ActiveTask editTask = {props.editTask} markTaskAsCompleted = {props.markTaskAsCompleted} deleteActiveTask = {props.deleteActiveTask} key = {task.id} id={task.id} title = {task.value}></ActiveTask>
       })}
-      <button onClick={addTask}>Add Task</button>
+      <button onClick={props.addTask}>Add Task</button>
     </div>
   );
 }
--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -8,44 +8,69 @@ class MainContent extends Component {
     this.markTaskAsCompleted = this.markTaskAsCompleted.bind(this);
     this.deleteActiveTask = this.deleteActiveTask.bind(this);
     this.updateActiveTask = this.updateActiveTask.bind(this);
+    this.addTask = this.addTask.bind(this);
     this.state = {
-      activeTasks : [
+      activeTasks: [
         { id: 1, value: "Task 1" },
         { id: 2, value: "Task 2" },
       ],
-      completedTasks : [
+      completedTasks: [
         { id: 1, value: "Task 3" },
         { id: 2, value: "Task 4" },
-      ]
-    }
+      ],
+    };
   }
 
   markTaskAsCompleted(id) {
     var completedTask = this.state.activeTasks.filter((item) => item.id === id);
-    this.setState({ completedTasks: [...this.state.completedTasks, completedTask[0]] })
-    this.deleteActiveTask(id);
+    if (completedTask[0].value) {
+      this.setState({
+        completedTasks: [...this.state.completedTasks, completedTask[0]],
+      });
+      this.deleteActiveTask(id);
+    }
+    else{
+      alert("Cannot mark empty task as completed")
+    }
   }
 
-  deleteActiveTask(id){
-    var updatedActiveTasks = this.state.activeTasks.filter((item) => item.id !== id);
-    this.setState({ activeTasks: updatedActiveTasks })
+  deleteActiveTask(id) {
+    var updatedActiveTasks = this.state.activeTasks.filter(
+      (item) => item.id !== id
+    );
+    this.setState({ activeTasks: updatedActiveTasks });
   }
 
-  updateActiveTask(id, value){
-    var updatedActiveTasks = this.state.activeTasks.map(obj=>{
-      if(obj.id === id){
-        return {...obj,value:value}
+  updateActiveTask(id, value) {
+    var updatedActiveTasks = this.state.activeTasks.map((obj) => {
+      if (obj.id === id) {
+        return { ...obj, value: value };
       }
-      return obj
+      return obj;
     });
-    this.setState({ activeTasks: updatedActiveTasks })
+    this.setState({ activeTasks: updatedActiveTasks });
+  }
+
+  addTask() {
+    this.setState({
+      activeTasks: [
+        ...this.state.activeTasks,
+        { id: this.state.activeTasks.length + 1, value: "" },
+      ],
+    });
   }
 
   render() {
     if (this.props.setActiveTasksPage) {
       return (
         <main className="main">
-          <ActiveTasks editTask = {this.updateActiveTask} markTaskAsCompleted = {this.markTaskAsCompleted} deleteActiveTask = {this.deleteActiveTask} tasks={this.state.activeTasks} />
+          <ActiveTasks
+            addTask={this.addTask}
+            editTask={this.updateActiveTask}
+            markTaskAsCompleted={this.markTaskAsCompleted}
+            deleteActiveTask={this.deleteActiveTask}
+            tasks={this.state.activeTasks}
+          />
         </main>
       );
     } else {
